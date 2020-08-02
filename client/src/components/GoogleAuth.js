5de4a9efb2eb9ad5be12d64a88e00c3bfcafb7e7
Erind Hoxha <erind_cbh@hotmail.com>
2020-08-02 14:46:58 +1200

    added googleAuth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,6 +2,18 @@ import React from 'react'
 import { NavLink } from 'reactstrap';
 import { Link } from "react-router-dom"
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', async () => {
+            await window.gapi.client.init({
+                clientId: "1",
+                scope: "email"
+            }, console.log('started')).then(response => {
+                console.log('Finished');
+                const auth = window.gapi.auth2.getAuthInstance()
+                console.log(auth);
+            })
+        });
+    }
     render() {
         return (
             <NavLink tag={Link} to="/login">Login</NavLink>
