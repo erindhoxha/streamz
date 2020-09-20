257aa9df90d358571d6815e787b7c4ccda020edc
Erind Hoxha <erind_cbh@hotmail.com>
2020-09-20 19:37:50 +1200

    adding edit stream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -32,6 +32,27 @@ export const SignOut = () => {
 
 export const CreateStream = (formValues) => async dispatch => {
     const response = await streams.post('/streams', formValues);
-
     dispatch({ type: CREATE_STREAM, payload: response.data })
-}
+};
+
+export const FetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data })
+};
+
+export const FetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data })
+};
+
+export const DeleteStream = (id) => async dispatch => {
+    const response = await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: response.data })
+};
+
+export const EditStream = (id) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, {
+
+    });
+    dispatch({ type: EDIT_STREAM, payload: response.data })
+};
