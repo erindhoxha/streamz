9d08efa6a2eff2120439d02645c89114caddaa21
erindhoxha <erind.cbh@gmail.com>
2021-06-13 18:31:43 +0200

    stream create, form changes

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,32 +1,10 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { CreateStream } from "../../actions";
 import Header from "../Header";
-import { Spinner } from 'reactstrap';
+import StreamForm from "../streams/StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, warning, touched }) {
-    if (touched && error) {
-      return <div className="alert alert-danger">{error}</div>;
-    }
-    if (touched && warning) {
-      return <div className="alert alert-warning">{warning}</div>;
-    }
-  }
-  renderInput = ({ input, label, meta }) => {
-    return (
-      <div className="form-group mt-2">
-        <label>{label}</label>
-        <input
-          autoComplete="off"
-          className="form-control form-control-md"
-          {...input}
-        />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
   onSubmit = (formValues) => {
     this.props.CreateStream(formValues);
   };
@@ -35,56 +13,12 @@ class StreamCreate extends React.Component {
       <div>
         <Header minified={true} />
         <div className="container mt-4">
-          <form className="" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <Field
-              name="title"
-              component={this.renderInput}
-              label="Enter title"
-            />
-            <Field
-              name="description"
-              component={this.renderInput}
-              label="Enter description"
-            />
-            <button className="btn btn-success" type="submit" disabled={this.props.submitting}>Submit                   
-              <Spinner animation="border" role="status">
-                Loading...
-              </Spinner>
-            </button>
-          </form>
+          <h3>Create form</h3>
+          <StreamForm onSubmit={this.onSubmit} />
         </div>
       </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    // only ran if the user did not enter a title
-    errors.title = "Title is required!";
-  }
-  if (!formValues.description) {
-    errors.description = "Description is required!";
-  }
-  return errors;
-};
-
-const warn = (formValues) => {
-  const warnings = {};
-  if (!formValues.title) {
-    warnings.title = "Hmm, that's kind of a short title.";
-  }
-  if (!formValues.description) {
-    warnings.description = "Description is required!";
-  }
-  return warnings;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate,
-  warn,
-})(StreamCreate);
-
-export default connect(null, { CreateStream })(formWrapped);
+export default connect(null, { CreateStream })(StreamCreate);
---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { Spinner } from 'reactstrap';
+
+class StreamForm extends React.Component {
+    renderError({ error, warning, touched }) {
+        if (touched && error) {
+            return <div className="alert alert-danger">{error}</div>;
+        }
+        if (touched && warning) {
+            return <div className="alert alert-warning">{warning}</div>;
+        }
+    }
+    renderInput = ({ input, label, meta }) => {
+        return (
+            <div className="form-group mt-2">
+                <label>{label}</label>
+                <input
+                    autoComplete="off"
+                    className="form-control form-control-md"
+                    {...input}
+                />
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+    onSubmit = (formValues) => {
+        this.props.onSubmit(formValues);
+    };
+    render() {
+        return (
+            <div>
+                <div className="container mt-4">
+                    <form className="" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                        <Field
+                            name="title"
+                            component={this.renderInput}
+                            label="Enter title"
+                        />
+                        <Field
+                            name="description"
+                            component={this.renderInput}
+                            label="Enter description"
+                        />
+                        <button className="btn btn-success" type="submit" disabled={this.props.submitting}>Submit
+                        <Spinner animation="border" role="status">
+                                Loading...
+                        </Spinner>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        // only ran if the user did not enter a title
+        errors.title = "Title is required!";
+    }
+    if (!formValues.description) {
+        errors.description = "Description is required!";
+    }
+    return errors;
+};
+
+const warn = (formValues) => {
+    const warnings = {};
+    if (!formValues.title) {
+        warnings.title = "Hmm, that's kind of a short title.";
+    }
+    if (!formValues.description) {
+        warnings.description = "Description is required!";
+    }
+    return warnings;
+};
+
+export default reduxForm({
+    form: "streamForm",
+    validate,
+    warn,
+})(StreamForm);
+
